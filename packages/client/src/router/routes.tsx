d02f6e4a57978ad8dcf1beddcfd5731524ae4211
Lobanov Kirill <lobanovk@list.ru>
2023-03-27 18:21:01 +0300

    feature(game-15): добавлен обработчик ошибок

--- packages/client/src/router/routes.tsx
+++ packages/client/src/router/routes.tsx
@@ -1,16 +1,23 @@
 import React from "react"
 import { createBrowserRouter } from "react-router-dom"
+import { ErrorWrapper } from '@/components/layouts/error-wrapper'
+import { ErrorPage } from '@/pages/ErrorPage'
 
 const App = React.lazy(() => import('@/pages'))
 const HelloWorld = React.lazy(() => import('@/pages/HelloWorld'))
 
+// Необходимо обернуть каждую новую страницу в ErrorWrapper за исключением страниц ошибок
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element:<ErrorWrapper><App /></ErrorWrapper>,
   },
   {
     path: '/hello',
-    element: <HelloWorld/>
+    element: <ErrorWrapper><HelloWorld/></ErrorWrapper>
+  },
+  {
+    path: '/500',
+    element: <ErrorPage />
   }
 ])
