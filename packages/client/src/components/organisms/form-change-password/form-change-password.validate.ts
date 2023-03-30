fc2300d3601273ea4a6fb3a5bd76f12543d59027
alheym <alheym.mw@gmail.com>
2023-03-30 10:34:29 +0300

    обновлены методы валидации в компонентах

--- packages/client/src/components/organisms/form-change-password/form-change-password.validate.ts
+++ packages/client/src/components/organisms/form-change-password/form-change-password.validate.ts
@@ -1,28 +1,19 @@
 // lib
 import * as yup from 'yup'
 
+import { passwordScheme, passwordNewScheme } from '../../../utils'
+
 // types
 import { FormPasswordValues } from './form-change-password.types'
 
 const schema: yup.SchemaOf<FormPasswordValues> = yup.object().shape({
 
-  passwordOld: yup
-    .string()
-    .trim()
-    .required('Пожалуйста введите ваш пароль')
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-      'Минимум 8 символов, должен включать цифры и латинские буквы'
-    ),
+  passwordOld: passwordScheme(
+    'Минимум 8 символов, должен включать цифры и латинские буквы'),
 
-  passwordNew: yup
-    .string()
-    .trim()
-    .required('Пожалуйста введите ваш пароль')
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-      'Минимум 8 символов, должен включать цифры и латинские буквы'
-    )
+  passwordNew: passwordNewScheme('passwordOld',
+    'Минимум 8 символов, должен включать цифры и латинские буквы'
+  )
 })
 
 export default schema
--- packages/client/src/components/organisms/form-signin/form-signin.validate.ts
+++ packages/client/src/components/organisms/form-signin/form-signin.validate.ts
@@ -1,22 +1,15 @@
 // lib
 import * as yup from 'yup'
 
+import { loginScheme, passwordScheme } from '../../../utils'
+
 // types
 import { FormSignInValues } from './form-signin.types'
 
 const schema: yup.SchemaOf<FormSignInValues> = yup.object().shape({
-  login: yup
-    .string()
-    .email('Введите валидный email-адрес')
-    .required('Это обязательное поле'),
-  password: yup
-    .string()
-    .trim()
-    .required('Пожалуйста введите ваш пароль')
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-      'Минимум 8 символов, должен включать цифры и латинские буквы'
-    ),
+  login: loginScheme('Может содержать только латинские буквы'),
+  password: passwordScheme(
+    'Минимум 8 символов, должен включать цифры и латинские буквы')
 })
 
 export default schema
