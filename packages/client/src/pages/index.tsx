4540742b5b116c5a0e752d1a64d34438548d2ade
erik <erikkhasanov@yandex.ru>
2023-04-03 18:39:14 +0300

    return test index

---
+++ packages/client/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+
+function App() {
+  useEffect(() => {
+    const fetchServerData = async () => {
+      const url = `http://localhost:${__SERVER_PORT__}`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchServerData()
+  }, [])
+  return <Navigate to={'/game'} replace />
+}
+
+export default App
