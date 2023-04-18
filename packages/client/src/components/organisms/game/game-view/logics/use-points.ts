83d755d890d4d156e8330c2d862b6324029144ed
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-04-18 10:09:10 +0300

    + fix use-points types

--- packages/client/src/components/organisms/game/game-view/logics/use-points.ts
+++ packages/client/src/components/organisms/game/game-view/logics/use-points.ts
@@ -1,7 +1,15 @@
+import { Coin } from '@/components/organisms/game/game-view/models/Coin'
+import { Enemy } from '@/components/organisms/game/game-view/models/Enemy'
+
 const COINCOST = 100
 const ENEMYCOST = 150
 
-export const Points = (props: any) => {
+type pointsProps = {
+  coins: Coin[]
+  enemies: Enemy[]
+}
+
+export const Points = (props: pointsProps) => {
   let coinsPoints = props.coins.length * COINCOST
   let enemyPoints = props.enemies.length * ENEMYCOST
 
