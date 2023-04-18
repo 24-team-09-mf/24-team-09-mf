dc24bae71d35dfcfe371b48b2d659094fe8415af
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-04-18 11:00:04 +0300

    + fix coin

--- packages/client/src/components/organisms/game/game-view/models/Coin.ts
+++ packages/client/src/components/organisms/game/game-view/models/Coin.ts
@@ -9,7 +9,6 @@ export class Coin extends Sprite {
   }
 
   switchSprite(name: string) {
-
     const animation = this.animations?.[name]
     if (this.image === animation?.image) return
 
@@ -21,23 +20,15 @@ export class Coin extends Sprite {
       this.frameBuffer = animation.frameBuffer || 2
     }
     setTimeout(() => {
-
       this.compeleAnimation = true
-
+      this.shouldDraw = false
     }, 260)
   }
 
   getCoin() {
     const getCoin = 'getCoin'
     this.switchSprite(getCoin)
-
-    if (this.compeleAnimation) {
-      this.shouldDraw = false
-      console.log('get coin, you should update score on one')
-    }
-
   }
-
 
   draw() {
     if (this.shouldDraw && !this.compeleAnimation) {
