80f3e427f15622f1785974cb0129a3bfbfc03316
leopard-work <zobnin.vladislav@yandex.ru>
2023-05-02 21:45:56 +0300

    + edit index

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -5,22 +5,23 @@ import { Provider } from 'react-redux'
 
 import App from './App'
 
-const startServiceWorker = () => {
-  if ('serviceWorker' in navigator && import.meta.env.MODE === 'production') {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js').then()
-    })
-  }
-}
+// const startServiceWorker = () => {
+//   if ('serviceWorker' in navigator && import.meta.env.MODE === 'production') {
+//     window.addEventListener('load', () => {
+//       navigator.serviceWorker.register('./sw.js').then()
+//     })
+//   }
+// }
+//
+// const requestNotificationPermission = () => {
+//   const permission = window.Notification.requestPermission()
+// }
+//
+// startServiceWorker()
+// requestNotificationPermission()
 
-const requestNotificationPermission = () => {
-  const permission = window.Notification.requestPermission()
-}
-
-startServiceWorker()
-requestNotificationPermission()
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
   <React.StrictMode>
     <Provider store={store}>
       <App />
