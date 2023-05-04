db697a869710ab02da9b02285d1bd9b0bc15a217
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-05-04 14:06:53 +0300

    + fullscreen

--- packages/client/src/components/organisms/game/game.styles.tsx
+++ packages/client/src/components/organisms/game/game.styles.tsx
@@ -5,14 +5,28 @@ import {
 } from '@/components/organisms/game/game.constants'
 import { Button } from '@/components'
 
-export const Wrapper = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
+export const Wrapper = styled.div((props: { fullScreen?: boolean }) => {
+  let main = `
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `
+  const full = `
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    top: 0;
+    left: 0;
+    z-index: 99999;
+  `
+  if (props.fullScreen) main += full
+  return main
+})
+
 export const MainView = styled.div`
-  position: relative;
+  position: absolute;
   width: ${WIDTH_VIEW}px;
   height: ${HEIGHT_VIEW}px;
 `
@@ -26,6 +40,7 @@ export const Content = styled.div`
   justify-content: center;
   padding: 0 115px;
 `
+
 export const BtnText = styled(Button)`
   font-size: 32px;
   padding: 0;
@@ -33,5 +48,24 @@ export const BtnText = styled(Button)`
 
   &:hover {
     text-decoration: none;
+  }
+`
+
+export const FullScreenBtn = styled.button`
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  z-index: 2;
+  border: none;
+  outline: none;
+  right: 20px;
+  top: 20px;
+  padding: 0;
+  background-color: transparent;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    opacity: 0.5;
   }
 `
