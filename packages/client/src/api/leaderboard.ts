e21cc1355a737e2d98449219876506a80bae21e9
alheym <alheym.mw@gmail.com>
2023-05-10 11:01:13 +0300

    добавлена обертка для запросов

--- packages/client/src/api/leaderboard.ts
+++ packages/client/src/api/leaderboard.ts
@@ -1,3 +1,4 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
 import http, { ApiEndpoints } from '@/api/base'
 import { TEAM_NAME } from '../constants/constants'
 import { LeaderboardElementProps } from '@/components/organisms/leaderboard/leaderboard-types'
@@ -8,20 +9,31 @@ export interface ILeaderboard {
   avatar?: string,
 }
 
-interface ILeaderboardResponse {
-  data: Array<{
-    data: LeaderboardElementProps,
-  }>,
-}
+export const addLeaderboardItem = createAsyncThunk('leaderboard/add',
+  async (data: ILeaderboard, { rejectWithValue }) => {
+    try {
+      await http.post(ApiEndpoints.Leaderboard.AddUser, {
+        data,
+        ratingFieldName: 'score',
+        teamName: TEAM_NAME,
+      })
+    } catch (e) {
+      return rejectWithValue('Ошибка при добавлении данных в таблицу лидеров')
+    }
+  }
+)
 
-export const addLeaderboardItem = (data: ILeaderboard) => http.post(ApiEndpoints.Leaderboard.AddUser, {
-  data,
-  ratingFieldName: 'score',
-  teamName: TEAM_NAME,
-})
-
-export const getLeaderboardList = (limit: number) => http.post(ApiEndpoints.Leaderboard.Leaderboard, {
-  ratingFieldName: 'score',
-  cursor: 0,
-  limit,
-}).then((response: ILeaderboardResponse) => response.data.map(({ data }) => data))
+export const getLeaderboardList = createAsyncThunk('leaderboard/getList',
+  async (limit: number, { rejectWithValue }) => {
+    try {
+      const response = await http.post(ApiEndpoints.Leaderboard.Leaderboard, {
+        ratingFieldName: 'score',
+        cursor: 0,
+        limit,
+      })
+      return response.data.map(({ data }: { data: LeaderboardElementProps }) => data)
+    } catch (e) {
+      return rejectWithValue('Ошибка при получении списка лидеров')
+    }
+  }
+)
