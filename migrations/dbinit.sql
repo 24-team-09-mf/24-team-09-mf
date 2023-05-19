b7921393ddf54e83a5f8d4420852bccb0bc5db84
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-05-19 16:43:07 +0300

    + fix dbinit

--- migrations/dbinit.sql
+++ migrations/dbinit.sql
@@ -38,4 +38,4 @@ CREATE TABLE posts (
 INSERT INTO categories (title, description) VALUES ('Сообщество 1', 'Обсуждение игры'), ('Поддержка', 'Возникли затруднения с установкой игры, игровым процессом или учетной записью? Приглашаем на форум службы поддержки.');
 INSERT INTO users (user_id, login, avatar) VALUES (532132, 'test 0001', 'https://ya-praktikum.tech/api/v2/resources/e1092287-ff24-410f-965d-0bf50bd35955/56c760b9-598e-42ad-acc8-d02afed05c21_61492-derevo_minimalizm_vodoem_holm.jpg'), (532135, 'test 0002', null);
 INSERT INTO topics (title, parent_id, user_id) VALUES ('Предложения по улучшению игры', 1, 1), ('Тема 222', 1, 2), ('Нужна помощь !!!', 2, 1);
-INSERT INTO posts (message, parent_id, user_id) VALUES ('Оставляйте свои предложения и вопросы', 1, 1), ('Текст для темы 2', 1, 2), ('Ничего не работает !!!', 2, 1);
+INSERT INTO posts (message, parent_id, user_id) VALUES ('Оставляйте свои предложения и вопросы', 1, 1), ('Текст для темы 2', 2, 2), ('Ничего не работает !!!', 3, 1);
