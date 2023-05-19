e89563e29d6d475982bfd49b6ce937d2ab387b9c
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-05-19 14:48:07 +0300

    + изменения dbinit

--- migrations/dbinit.sql
+++ migrations/dbinit.sql
@@ -1,22 +1,41 @@
 CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
     description TEXT,
-    posts_count INTEGER NOT NULL default 0,
-    last_message INTEGER,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
+    "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL default NOW()
+);
+
+CREATE TABLE users (
+    id SERIAL,
+    user_id INTEGER PRIMARY KEY,
+    login TEXT NOT NULL,
+    avatar TEXT,
     "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
     "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL default NOW()
 );
 
 CREATE TABLE topics (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    author INTEGER,
+    title TEXT NOT NULL,
+    parent_id INTEGER,
+    user_id INTEGER,
     "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
     "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
-    parent_id INTEGER,
-    CONSTRAINT parent_fk FOREIGN KEY (parent_id) REFERENCES categories (id)
+    FOREIGN KEY (parent_id) REFERENCES categories (id) ON DELETE CASCADE
 );
 
-INSERT INTO categories (name, description) VALUES ('Сообщество', 'Обсуждение игры'), ('Поддержка', 'Возникли затруднения с установкой игры, игровым процессом или учетной записью? Приглашаем на форум службы поддержки.');
-INSERT INTO topics (name, parent_id) VALUES ('Тема 111', 1), ('Тема 222', 2);
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    message TEXT NOT NULL,
+    parent_id INTEGER,
+    user_id INTEGER,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
+    "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL default NOW(),
+    FOREIGN KEY (parent_id) REFERENCES topics (id) ON DELETE CASCADE
+);
+
+INSERT INTO categories (title, description) VALUES ('Сообщество 1', 'Обсуждение игры'), ('Поддержка', 'Возникли затруднения с установкой игры, игровым процессом или учетной записью? Приглашаем на форум службы поддержки.');
+INSERT INTO users (user_id, login, avatar) VALUES (532132, 'test 0001', 'https://ya-praktikum.tech/api/v2/resources/e1092287-ff24-410f-965d-0bf50bd35955/56c760b9-598e-42ad-acc8-d02afed05c21_61492-derevo_minimalizm_vodoem_holm.jpg'), (532135, 'test 0002', null);
+INSERT INTO topics (title, parent_id, user_id) VALUES ('Предложения по улучшению игры', 1, 1), ('Тема 222', 1, 2), ('Нужна помощь !!!', 2, 1);
+INSERT INTO posts (message, parent_id, user_id) VALUES ('Оставляйте свои предложения и вопросы', 1, 1), ('Текст для темы 2', 1, 2), ('Ничего не работает !!!', 2, 1);
