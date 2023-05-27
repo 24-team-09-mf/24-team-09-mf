32a35ca469f1b362e97f9ec757c54f7f21b253c3
oalbukova <o.albukova@gmail.com>
2023-05-27 11:36:21 +0300

    ид

--- packages/server/src/routes/themes.ts
+++ packages/server/src/routes/themes.ts
@@ -1,13 +1,15 @@
 import { Router, type Request, type Response } from 'express'
 import { getTheme, postTheme } from '../controllers/themes'
+import { checkUser } from '../utils/checkUser'
 
 export const themeRouter = Router()
   .get(
     '/',
-    async (_: Request, res: Response, next): Promise<Response | void> => {
+    async (req: Request, res: Response, next): Promise<Response | void> => {
+      const {  user } = req.body
       try {
-        const userId = res.locals.user.id
-        const userTheme = await getTheme(userId)
+        const userId = await checkUser(user)
+        const userTheme = await getTheme(userId.dataValues.id)
 
         if (userTheme?.theme.theme_name) {
           return res.status(200).json(userTheme.theme.theme_name)
@@ -22,14 +24,15 @@ export const themeRouter = Router()
   .post(
     '/',
     async (req: Request, res: Response, next): Promise<Response | void> => {
+      const {  user } = req.body
       try {
         const {
           data: { theme_name },
         } = req.body
 
-        const userId = res.locals.user.id
+        const userId = await checkUser(user)
 
-        const userTheme = await postTheme(theme_name, userId)
+        const userTheme = await postTheme(theme_name, userId.dataValues.id)
 
         if (userTheme) {
           return res.status(201).json(userTheme)
