9609e9f0ed37854b411692fb53f0d4f48ef403cd
oalbukova <o.albukova@gmail.com>
2023-06-01 00:20:22 +0300

    no store

--- packages/client/src/store/rootReducer.ts
+++ packages/client/src/store/rootReducer.ts
@@ -2,11 +2,10 @@ import { combineReducers } from '@reduxjs/toolkit'
 import userSlice from './user/userSlice'
 import gameSlice from './game/gameSlice'
 import leaderboarSlice from './leaderboard/leaderboarSlice'
-import themeSlice from "./siteTheme/slice"
 
 export const rootReducer = combineReducers({
   user: userSlice,
   game: gameSlice,
   leaderboard: leaderboarSlice,
-  theme: themeSlice,
+
 })
--- packages/client/src/store/themeStore.ts
+++
@@ -1,7 +0,0 @@
-import { useAppSelector } from './hooks'
-
-export const themeStore = () => {
-  const themes = useAppSelector(state => state.theme.themes)
-  const userTheme = useAppSelector(state => state.theme.userTheme)
-  return { themes, userTheme }
-}
