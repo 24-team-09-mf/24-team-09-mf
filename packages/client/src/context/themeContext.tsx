0783ef4a1869ad13b59a56c12911291be622175c
alheym <alheym.mw@gmail.com>
2023-06-08 13:54:00 +0300

    fix

--- packages/client/src/context/themeContext.tsx
+++ packages/client/src/context/themeContext.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState, createContext } from 'react'
 import { addUserTheme, getUserTheme } from '@/api/userTheme'
 import { userStore } from '@/store'
 
@@ -14,7 +14,7 @@ type ThemeContextType = {
   toggleTheme: () => void
 }
 
-const ThemeContext = React.createContext<ThemeContextType>(
+const ThemeContext = createContext<ThemeContextType>(
   {} as ThemeContextType
 )
 
