fb23602569c8f5ce906fa6dde105685daa142133
Lobanov Kirill <lobanovk@list.ru>
2023-06-21 22:40:46 +0300

    Revert "feat(*): for test oauth"
    This reverts commit 61f5bdd853496d09e7e3bfb95ba058feb6635638.

--- packages/client/src/store/user/oauth/actions.ts
+++ packages/client/src/store/user/oauth/actions.ts
@@ -8,25 +8,15 @@ export const oAuthGetServiseId = createAsyncThunk(
   'oauth/oAuthGetServiseId',
   async (_, { rejectWithValue }) => {
     try {
-      console.log('constants start: ------------')
-      console.table({ RedirectUrl, OAuthUrl })
-      console.log('constants end: ------------')
       const { data } = await http.get<ServiceId>(ApiEndpoints.Auth.ServiceId, {
         params: { redirect_uri: RedirectUrl },
       })
-
-      console.log("data start: ----------------")
-      console.table(data);
-      console.log("data end: --------------------")
 
       const url = new URL(OAuthUrl)
 
       url.searchParams.set('client_id', data.service_id)
       RedirectUrl && url.searchParams.set('redirect_uri', RedirectUrl)
-      console.log('url start: ----------')
-      console.log(url);
-      console.log('url end: -----------')
-      // window.location.href = url.href
+      window.location.href = url.href
     } catch (e) {
       return rejectWithValue('OAuth авторизация: Ошибка получения id')
     }
